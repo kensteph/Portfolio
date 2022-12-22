c9e3aa1f97705115fc1d9bfd4c2b288555748d89
edahigure <edahigure@gmail.com>
2022-12-22 10:27:43 -0600

    Target  name and the message input

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,9 @@
 //  Target the form and its children
+
 const contactForm = document.querySelector('#contact-form');
+const userName = document.querySelector('#name');
 const email = document.querySelector('#email');
+const message = document.querySelector('#message');
 const errorMessage = document.querySelector('#error-msg');
 
 contactForm.addEventListener('submit', (e) => {
